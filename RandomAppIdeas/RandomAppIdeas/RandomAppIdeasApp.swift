614f68967d734bd538ba2244caf4f9c0c5acfb17
Jordan Morgan <the_morgan_fam@yahoo.com>
2021-07-09 12:43:14 -0500

    TodayTomorrowSomeday

---
+++ RandomAppIdeas/RandomAppIdeas/RandomAppIdeasApp.swift
@@ -0,0 +1,18 @@
+//
+//  RandomAppIdeasApp.swift
+//  RandomAppIdeas
+//
+//  Created by Jordan Morgan on 7/9/21.
+//
+
+import SwiftUI
+
+@main
+struct RandomAppIdeasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowToolbarStyle(.unifiedCompact(showsTitle: false))
+    }
+}
